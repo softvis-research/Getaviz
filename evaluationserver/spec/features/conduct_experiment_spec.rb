a28f81fd4b4399ec4fc89d91cafdf8af3bb2fe8b
Jan Schilbach <jan.schilbach@googlemail.com>
2018-04-25 21:48:16 +0200

    begin of implementation of test for Conduct Experiment

--- evaluationserver/spec/features/conduct_experiment_spec.rb
+++ evaluationserver/spec/features/conduct_experiment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "conduct experiment", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  
+  scenario "with static text" do
+
+  end
+
+  scenario "with scene" do
+
+
+  end
+
+  scenario "with scene test" do
+
+
+  end
+  
 end
