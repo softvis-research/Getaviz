4eaf5f32c849d74c3ee7021e6838676774b5568f
Jan Schilbach <jan.schilbach@googlemail.com>
2018-04-30 22:23:51 +0200

    Rudimentary Feature Spec for conducting a simple experiment

--- evaluationserver/spec/features/conduct_experiment_spec.rb
+++ evaluationserver/spec/features/conduct_experiment_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.feature "conduct experiment", type: :feature do
   
   scenario "with static text" do
-
+    @experiment = create(:experiment_with_static_text)
+    visit start_path(:experiment_hash => @experiment.id_hash)
+    expect(page).to have_content(Nokogiri::XML(@experiment.steps.first.textual_elements.first.html).content)
+    find('input[name="commit"]').click
   end
 
   scenario "with scene" do
@@ -13,7 +16,11 @@ RSpec.feature "conduct experiment", type: :feature do
 
   scenario "with scene test" do
 
+  end
+
+  scenario "with multiple scenes test" do
 
   end
+
   
 end
