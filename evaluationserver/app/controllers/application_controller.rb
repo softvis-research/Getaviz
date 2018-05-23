6c37efd7c84d4af43f6936882e64abb073e4329a
Jan Schilbach <jan.schilbach@googlemail.com>
2018-05-23 21:21:38 +0200

    Skipping Setting of cookies except admin

--- evaluationserver/app/controllers/application_controller.rb
+++ evaluationserver/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
   before_action :set_cache_headers
 
   CREDENTIALS = {
@@ -47,6 +47,7 @@ class ApplicationController < ActionController::Base
     response.headers["Cache-Control"] = "no-cache, no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    request.session_options[:skip] = true
   end
 
 end
