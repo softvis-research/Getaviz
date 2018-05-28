47db1278527e9d0e65c6f8e8c0bca8c46984c58f
Jan Schilbach <jan.schilbach@googlemail.com>
2018-05-28 23:06:07 +0200

    ExperimentsController Rudimentary Tests

--- evaluationserver/spec/controllers/experiments_controller_spec.rb
+++ evaluationserver/spec/controllers/experiments_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ExperimentsController, type: :controller do
+  include BasicAuthHelper
   describe "GET conduct" do
     it "Raises controlled error on wrong experiment" do
       @experiment = create(:experiment_with_static_text)
@@ -8,4 +9,49 @@ RSpec.describe ExperimentsController, type: :controller do
       expect(response).to have_http_status(:error)
     end
   end
+
+  describe "GET edit" do
+    it "opens edit page" do
+      login_as_admin
+      @experiment = create(:experiment_with_static_text)
+      get 'edit', :params => {:id => @experiment.id} ## this is the wrong type of hash
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+
+  describe "POST update" do
+    it "can update the title" do
+      login_as_admin
+      @experiment = create(:experiment_with_static_text)
+      new_title = Faker::Name.name
+      patch 'update', :params => {:id => @experiment.id, :experiment => {:title => new_title}} ## this is the wrong type of hash
+      expect(response).to have_http_status(:found)
+      @experiment.reload
+      expect(@experiment.title).to eq(new_title)
+      
+    end
+  end
+
+
+  describe "DELETE destroy" do
+    it "can update the title" do
+      login_as_admin
+      @experiment = create(:experiment_with_static_text)
+      experiment_id = @experiment.id
+      expect(Experiment.where(:id => experiment_id).length).to eq(1)
+      
+      new_title = Faker::Name.name
+      delete 'destroy', :params => {:id => @experiment.id} ## this is the wrong type of hash
+      expect(response).to have_http_status(:found)
+      expect(Experiment.where(:id => experiment_id).length).to eq(0)
+      
+    end
+  end
+
+
+
+
+
+  
 end
--- evaluationserver/spec/factories/questions.rb
+++ evaluationserver/spec/factories/questions.rb
@@ -1,5 +1,23 @@
 FactoryBot.define do
   factory :question do
+    factory :number_question do
+      question_text "Welche Nummer wird gesucht?"
+
+      factory :number_question_with_one_answer_possibility do
+        after(:create) do |number_question_with_one_answer_possibility, evaluator|
+          create_list(:answer_possibility_collection, 1, question: number_question_with_one_answer_possibility)
+        end
+      end
+      
+      
+      
+    end
+
+    factory :text_question do
+      question_text "Welcher Text wird gesucht?"
+      
+    end
+    
     
   end
 end
