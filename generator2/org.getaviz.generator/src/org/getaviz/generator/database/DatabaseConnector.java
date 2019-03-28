b9cc58af10e9f4c2695ee99a24bfcaf91343b98c
Unknown <karuri@web.de>
2019-03-28 15:40:57 +0100

    Add convenient methods to modify database

--- generator2/org.getaviz.generator/src/org/getaviz/generator/database/DatabaseConnector.java
+++ generator2/org.getaviz.generator/src/org/getaviz/generator/database/DatabaseConnector.java
@@ -8,6 +8,7 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Transaction;
 import org.neo4j.driver.v1.TransactionWork;
+import org.neo4j.driver.v1.types.Node;
 
 public class DatabaseConnector implements AutoCloseable {
 	private final Driver driver;
@@ -39,11 +40,42 @@ public class DatabaseConnector implements AutoCloseable {
 		}
 	}
 	
+	public Node addNode(String statement, String parameterName) {
+		Node result = null;
+		try (Session session = driver.session()) {
+            try (Transaction tx = session.beginTransaction()) {
+                result = tx.run(statement + " RETURN " + parameterName).next().get(parameterName).asNode();
+                tx.success();  // Mark this write as successful.
+            }
+        }
+		return result;
+	}
+	
+	public void addRelationship(Long from, Long to, String relationship) {
+		try (Session session = driver.session(AccessMode.WRITE)) {
+			session.writeTransaction(new TransactionWork<Integer>() {
+				@Override 
+				public Integer execute(Transaction tx) {
+						tx.run(String.format("MATCH (from),(to) WHERE ID(from) = %d AND ID(to) = %d CREATE (from)-[r:%s]->(to)", from, to, relationship));
+					return 1;
+				}
+			});		
+		}
+	}
+	
 	public StatementResult executeRead(String statement) {
 		try (Session session = driver.session(AccessMode.READ)) {
 			StatementResult result = session.run(statement);
 			return result;
 		}
+	}
+	
+	public Node getVisualizedEntity(Long id) {
+		return executeRead("MATCH (n)-[:VISUALIZES]->(e) WHERE ID(n) = " + id + " RETURN e").single().get("e").asNode();
+	}
+	
+	public Node getPosition(Long id) {
+		return executeRead("MATCH (n)-[:HAS]->(p:Position) WHERE ID(n) = " + id + " RETURN p").single().get("p").asNode();
 	}
 
 	@Override
