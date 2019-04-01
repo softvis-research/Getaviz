47c98942a95b6a178494a99d4b3cbe95ffc16133
jens <jt23coqi@studserv.uni-leipzig.de>
2019-04-01 17:11:37 +0200

    Debug Stuff removed

--- ui/scripts/GenerationForm/GenerationFormController.js
+++ ui/scripts/GenerationForm/GenerationFormController.js
@@ -1203,58 +1203,36 @@ var generationFormController = (function() {
 					rules: [
 						{ input: manual_input_name, message: 'Please enter an input.name!', action: 'keyup', position: 'top:0,15', rule: 'required' },
 						{ input: manual_input_files, message: 'Please enter the path to your input.files!', action: 'keyup, focus, blur, valuechanged', position: 'top:0,15', rule: 'required' },
-						{ input: manual_city_package_color_start, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_package_color_end, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_class_color_start, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_class_color_end, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_class_color, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_blue, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_aqua, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_light_green, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_dark_green, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_yellow, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_orange, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_red, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_pink, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_violet, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_light_grey, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_dark_grey, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_white, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_city_color_black, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_rd_color_class, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_rd_color_data, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_rd_color_method, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-						{ input: manual_rd_color_namespace, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: 'length=7,7',},
-
-
-						// { input: manual_city_package_color_start, message: 'Please enter a HEX Color!', action: 'valuechanged', position: 'top:0,15', rule: function() {
-
-							// var hex_format = "/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/";
-							// console.log('custom function');
-							// console.log(manual_city_package_color_start);
-							
-							// if(manual_city_package_color_start.contains('#') == true)
-								// return true;
-							// else
-								// return false;							
-							
-							// return true;
-							
-							// }
-						// },
+						{ input: manual_city_package_color_start, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_package_color_end, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_class_color_start, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_class_color_end, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_class_color, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_blue, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_aqua, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_light_green, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_dark_green, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_yellow, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_orange, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_red, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_pink, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_violet, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_light_grey, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_dark_grey, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_white, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_city_color_black, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_rd_color_class, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_rd_color_data, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_rd_color_method, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
+						{ input: manual_rd_color_namespace, message: 'Please enter a HEX Color!', action: 'keyup, valuechanged', position: 'top:0,15', rule: 'length=7,7',},
 					]
-				});
-				
+				});				
 				
 				// Submit Form Data 
 				var btn = settingsForm.jqxForm('getComponentByName', 'submitButton');
 				btn.on('click', function () {
-					
 					$('#settingsForm').jqxValidator('validate');
-					//console.log('.bind click validate done');
-					
 					settingsForm.jqxForm('submit', "http://" + BACKEND +":8080", "_self", 'POST');
-					//console.log('bind click submit done');
 				});
             }
         });
