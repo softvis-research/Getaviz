43bdf9f5784886ec5d4cd58af8740acd1b35e94e
stefanwebo <webo@nurfuerspam.de>
2019-09-05 15:55:08 +0200

    - changed everything back to aframe v0.9.1

--- ui/index.php
+++ ui/index.php
@@ -24,9 +24,9 @@
     $metaDataJsonUrl = $modelUrl . "/metaData.json";
 
     if (isset($_GET["aframe"]) && $_GET["aframe"] == 'true') {
-        $loadFramework = "<script src=\"node_modules/aframe/dist/aframe-v0.9.2.min.js\"></script>";
+        $loadFramework = "<script src=\"node_modules/aframe/dist/aframe-v0.9.1.min.js\"></script>";
         $loadVisualizationSpecificScripts = <<<'EOT'
-        <script src="node_modules/aframe/dist/aframe-v0.9.2.min.js"></script>
+        <script src="node_modules/aframe/dist/aframe-v0.9.1.min.js"></script>
         <script type="text/javascript" src="scripts/AframeCanvasManipulator.js"></script>
         <script type="text/javascript" src="scripts/AframeActionController.js"></script>
 	    <script type="text/javascript" src="scripts/RelationConnector/AframeRelationConnectorController.js"></script>
