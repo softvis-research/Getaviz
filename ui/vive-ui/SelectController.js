e0810f1a3dea305d06cb39261d4c94a8f554e053
Jonathan Lange <jonathanlange@web.de>
2019-09-06 14:12:50 +0200

    add click-listener

--- ui/vive-ui/SelectController.js
+++ ui/vive-ui/SelectController.js
@@ -1,39 +1,77 @@
+/**
+ * Sets EventListener for all <a-box> tags. 
+ * The Eventlisteners are:
+ *  - mouseenter -> when an entity will be hovered
+ *  - mouseleave -> when an entity will not be hovered anymore
+ *  - click      -> when an entity will be selected
+ */
 (function(){
     setTimeout(() => {
+        model.initialize(metaData);
         var boxes = document.querySelectorAll('a-box')
         boxes.forEach( (box) => {
             var color = box.getAttribute('color')
+            var entity = model.getEntityById(box.id)
             box.addEventListener('mouseenter', (evt) => {
-                box.setAttribute('color', '#FF0000')
+                var entities = []
+                entities.push(entity)
+                
+		        canvasManipulator.highlightEntities(entities, 'darkred');
+                model.getEntityById(box.id).hovered = true
                 showTooltip(getItemStringFromID(box.id))
             })
             box.addEventListener('mouseleave', (evt) => {
-                box.setAttribute('color', color)
+                var entities = []
+                entities.push(entity)
+		        canvasManipulator.unhighlightEntities(entities, color);
                 hideTooltip()
             })
+            box.addEventListener('click', (evt)=> {
+                var entities = []
+                entities.push(entity)
+                var applicationEvent = {
+                    sender		: canvasSelectController,
+                    entities	: entities
+                };
+                entity.selected = true
+                canvasSelectController.onEntitySelected(applicationEvent)
+                console.log(entity);
+            })
         })
+        
     }, 1000);
 })()
 
-function showTooltip(name) {
+/**
+ * Makes the tooltip element in the UI visible and shows the itemString inside
+ * 
+ * @param {String} itemString should contain the type and the name of the selected Item
+ */
+function showTooltip(itemString) {
     document.getElementById('vive-tooltip').innerHTML = 
-        `<a-entity text="value: ${name}; align: center;" position="0 0 0"></a-entity>`
+        `<a-entity text="value: ${itemString}; align: center;" position="0 0 0"></a-entity>`
     document.getElementById('vive-tooltip').object3D.visible = true
 }
 
+/**
+ * Hides the tooltip element in the UI 
+ */
 function hideTooltip() {
     document.getElementById('vive-tooltip').object3D.visible = false
 }
 
+/**
+ * Looks for item for specific id and returns a string for the tooltip
+ * 
+ * @param {String} id id of the selected Element
+ */
 function getItemStringFromID(id) {
-    var object = metaData.find( (item) => {
-        return item.id === id
-    })
+    var object = model.getEntityById(id)
     var name = object.name
     var type = object.type
-    if (type === "FAMIX.Namespace") {
+    if (type === "Namespace") {
         return `Package: ${name}`
-    } else if (type === "FAMIX.Class") {
+    } else if (type === "Class") {
         return `Class: ${name}`
     }
 }
--- ui/vive-ui/vive-ui.js
+++ ui/vive-ui/vive-ui.js
@@ -8,14 +8,14 @@ AFRAME.registerComponent("vive-package-explorer", {
 AFRAME.registerComponent("vive-legend", {
     init: function () {
         this.el.innerHTML = 
-            `<a-entity text="value: Legend;" position="0.3 0.2 0"></a-entity>`
-        this.el.addEventListener('mouseenter', function (evt) {
-            console.log(evt.detail.intersection.point);
-        });
+            `<a-entity text="value: Legend;" position="0.3 0.2 0"></a-entity>`+
+            `<a-image src="#legend-package" position="-0.15 0.1 0" width="0.05" height="0.05"></a-image>`+
+            `<a-entity text="value: package;" position="0.4 0.1 0"></a-entity>`+
+            `<a-image src="#legend-type" position="-0.15 0.05 0" width="0.05" height="0.05"></a-image>`+
+            `<a-entity text="value: type;" position="0.4 0.05 0"></a-entity>`
     }
 })
 
 AFRAME.registerComponent("vive-tooltip", {
     init: function () {}
 })
-
