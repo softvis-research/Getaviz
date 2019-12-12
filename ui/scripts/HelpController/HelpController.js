3d21508fe1cbdd26ccd3ecc7ad52f50cab4490fd
Sophia <gaoxuefei55@gmail.com>
2019-12-12 11:51:55 +0100

    Improve HelpController (function)

--- ui/scripts/HelpController/HelpController.js
+++ ui/scripts/HelpController/HelpController.js
@@ -34,106 +34,92 @@ var helpController = (function() {
             cssLink.href = "scripts/HelpController/helpPopup.css";
             document.getElementsByTagName("head")[0].appendChild(cssLink);
             
-            if(controllerConfig.metaphor=="City original") {
-                $.getScript("scripts/HelpController/metaphorCityoriginal.js", function(){   
-                     helpPopupContent();
-                });
-            };
-            
-            if(controllerConfig.metaphor=="City bricks") {
-                $.getScript("scripts/HelpController/metaphorCitybricks.js", function(){  
-                    helpPopupContent();
-                }); 
-            };
-            
-            if(controllerConfig.metaphor=="City floor") {
-                $.getScript("scripts/HelpController/metaphorCityfloor.js", function(){  
-                   helpPopupContent();
-                }); 
-            };
-
-            if(controllerConfig.metaphor=="RD") {
-                $.getScript("scripts/HelpController/metaphorRD.js", function(){  
-                   helpPopupContent();
-                }); 
-            };
+            createPopup();
+            createTabs();
        });
     }
-    
-    function jqxTabsPopup(){
-            var jqxTabsPopup = `
-             <ul class='tabsUl helpController'>
-                <li>
-                     <div>
-                        <img src='scripts/HelpController/images/category.png'>
-                        <div class='tabsUl_div helpController'>Legend</div>
-                     </div>
-                </li>
-                <li>
-                     <div>
-                         <img src='scripts/HelpController/images/group_work.png'>
-                         <div class='tabsUl_div helpController'>Relationships</div>
-                     </div>
-                 </li>
-                 <li>
-                     <div >
-                         <img src='scripts/HelpController/images/mouse.png'>
-                         <div class='tabsUl_div helpController'>Navigation</div>
-                     </div>
-                 </li>
-             </ul>
-             <div></div>
-             <div></div>
-             <div></div>`;
-            
-         return jqxTabsPopup;     
-    }; 
 
     function createPopup(){
-        var helpPopupDiv= "<div id='helpPopupDiv'></div>";
-        $("#DisplayWindow").remove();
-        var loadPopup = application.createPopup("Help",  
-                        helpPopupDiv, "DisplayWindow");
-                        document.body.appendChild(loadPopup);
+        var popup = application.createPopup("Help",  createJqxTabsHTML(), "DisplayWindow");
+        document.body.appendChild(popup);
         $("#DisplayWindow").css("display", "block").jqxWindow({
-                                theme: "metro",
-                                width: 565,
-                                height: 645,
-                                isModal: true,
-                                autoOpen: true,
-                                resizable: false
+            theme: "metro",
+            width: 565,
+            height: 645,
+            isModal: true,
+            autoOpen: true,
+            resizable: false
         });
-        return loadPopup; 
     };
     
-    function popupContent(){
-        return "<div id='helpPopupTabs'>" + jqxTabsPopup() + "</div>";
-    };
-            
     function createTabs(){
         $('#helpPopupTabs').jqxTabs({ 
-                    theme:'metro',
-                    width: 550,
-                    height: 600,
-                    position:'top'
+            theme:'metro',
+            width: 550,
+            height: 600,
+            position:'top'
         });
 
-        $('#helpPopupTabs').jqxTabs('setContentAt', 0, variantMetaphor.legend());
-        $('#helpPopupTabs').jqxTabs('setContentAt', 1, variantMetaphor.relationships());
-        $('#helpPopupTabs').jqxTabs('setContentAt', 2, variantNavigation.navigation());
-        
-    };
+        if(controllerConfig.metaphor=="City original") {
+            $.getScript("scripts/HelpController/metaphorCityoriginal.js", function(){   
+                createPopupContent()
+            });
+        };
+            
+        if(controllerConfig.metaphor=="City bricks") {
+            $.getScript("scripts/HelpController/metaphorCitybricks.js", function(){  
+                createPopupContent()
+            });
+        };
+            
+        if(controllerConfig.metaphor=="City floor") {
+            $.getScript("scripts/HelpController/metaphorCityfloor.js", function(){  
+                createPopupContent()
+            });
+        };
 
-    function createPopupAndTabs(){
-        createPopup();
-        document.getElementById('helpPopupDiv').innerHTML= popupContent();
-        createTabs();
+        if(controllerConfig.metaphor=="RD") {
+            $.getScript("scripts/HelpController/metaphorRD.js", function(){  
+                createPopupContent()
+            }); 
+        };
     };
     
-    function helpPopupContent(){
-        $.getScript("scripts/HelpController/navigationContent.js", function(){
-            createPopupAndTabs();
+    function createPopupContent(){
+        $('#helpPopupTabs').jqxTabs('setContentAt', 0, variantMetaphor.legend());
+        $('#helpPopupTabs').jqxTabs('setContentAt', 1, variantMetaphor.relationships());
+        $.getScript("scripts/HelpController/navigationContent.js", function(){ 
+            $('#helpPopupTabs').jqxTabs('setContentAt', 2, variantNavigation.navigation());   
         });
+    };
+
+    function createJqxTabsHTML(){
+        return `
+            <div id='helpPopupTabs'>
+                <ul class='tabsUl helpController'>
+                    <li>
+                        <div>
+                            <img src='scripts/HelpController/images/category.png'>
+                            <div class='tabsUl_div helpController'>Legend</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src='scripts/HelpController/images/group_work.png'>
+                            <div class='tabsUl_div helpController'>Relationships</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div >
+                            <img src='scripts/HelpController/images/mouse.png'>
+                            <div class='tabsUl_div helpController'>Navigation</div>
+                        </div>
+                    </li>
+                </ul>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>`;
     };
     
     return {
