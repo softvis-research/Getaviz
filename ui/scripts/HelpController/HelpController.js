4a6d619eaa224720bfd6a12382947ba44697414d
Sophia <gaoxuefei55@gmail.com>
2019-12-12 09:14:44 +0100

    Improve HelpController.js

--- ui/scripts/HelpController/HelpController.js
+++ ui/scripts/HelpController/HelpController.js
@@ -4,84 +4,10 @@ var helpController = (function() {
 	};
     
     function initialize(setupConfig){   
-        
         application.transferConfigParams(setupConfig, controllerConfig);
     }
-    
-    function tabsUl(){
-            var tabsUl = `
-             <ul class='tabsUl helpController'>
-                <li>
-                     <div>
-                         <img src='scripts/HelpController/images/group_work.png'>
-                         <div class='tabsUl_div helpController'>Relationships</div>
-                     </div>
-                 </li>
-                 <li>
-                     <div >
-                         <img src='scripts/HelpController/images/mouse.png'>
-                         <div class='tabsUl_div helpController'>Navigation</div>
-                     </div>
-                 </li>
-             </ul>`;
-            
-         return tabsUl;     
-    }; 
-    
-    function addLegendTab(){
-            var addLegend = `
-                <div class='tabsUl helpController'>
-                        <img src='scripts/HelpController/images/category.png'>
-                        <div class='tabsUl_div helpController'>Legend</div>
-                </div>`;
-        return addLegend; 
-    };
-    
-    function createPopup(){
 
-        var helpPopupDiv= "<div id='helpPopupDiv'></div>";
-        $("#DisplayWindow").remove();
-        var loadPopup = application.createPopup("Help",  
-                        helpPopupDiv, "DisplayWindow");
-                        document.body.appendChild(loadPopup);
-        $("#DisplayWindow").css("display", "block").jqxWindow({
-                                theme: "metro",
-                                width: 565,
-                                height: 645,
-                                isModal: true,
-                                autoOpen: true,
-                                resizable: false
-        });
-        return loadPopup; 
-    };
-    
-    function popupContent(){
-        var helpPopup = "<div id='helpPopupTabs'>" + tabsUl() + variantMetaphor.relationships() + variantNavigation.navigation() + "</div>";
-        
-        return helpPopup;
-    };
-            
-    function createTabs(){
-        
-        $('#helpPopupTabs').jqxTabs({ 
-                    theme:'metro',
-                    width: 550,
-                    height: 600,
-                    position:'top'
-        });
-
-        $('#helpPopupTabs').jqxTabs('addFirst', addLegendTab(), variantMetaphor.legend());
-    };
-
-    function createPopupAndTabs(helpPopup){
-         
-          createPopup();
-          document.getElementById('helpPopupDiv').innerHTML= helpPopup;
-          createTabs();
-     };
-    
 	function activate(){
-
         var id = "jqxHelpButton";
         var jqxHelpButtonType = "button";
         var jqxHelpButton = document.createElement("BUTTON");
@@ -102,7 +28,6 @@ var helpController = (function() {
         });
 
        $("#jqxHelpButton").on('click', function (){
-            
             var cssLink = document.createElement("link");
             cssLink.type = "text/css";
             cssLink.rel = "stylesheet";
@@ -111,38 +36,105 @@ var helpController = (function() {
             
             if(controllerConfig.metaphor=="City original") {
                 $.getScript("scripts/HelpController/metaphorCityoriginal.js", function(){   
-                     $.getScript("scripts/HelpController/navigationContent.js", function(){
-                       createPopupAndTabs(popupContent());
-                    });
+                     helpPopupContent();
                 });
             };
             
             if(controllerConfig.metaphor=="City bricks") {
                 $.getScript("scripts/HelpController/metaphorCitybricks.js", function(){  
-                    $.getScript("scripts/HelpController/navigationContent.js", function(){
-                       createPopupAndTabs(popupContent());
-                    });
+                    helpPopupContent();
                 }); 
             };
             
             if(controllerConfig.metaphor=="City floor") {
                 $.getScript("scripts/HelpController/metaphorCityfloor.js", function(){  
-                   $.getScript("scripts/HelpController/navigationContent.js", function(){
-                       createPopupAndTabs(popupContent());
-                    });
+                   helpPopupContent();
                 }); 
             };
 
             if(controllerConfig.metaphor=="RD") {
-                $.getScript("scripts/HelpController/metaphorRD.js", function(){
-                    $.getScript("scripts/HelpController/navigationContent.js", function(){
-                       createPopupAndTabs(popupContent());
-                    });
-                });
+                $.getScript("scripts/HelpController/metaphorRD.js", function(){  
+                   helpPopupContent();
+                }); 
             };
-           
-      });
+       });
     }
+    
+    function jqxTabsPopup(){
+            var jqxTabsPopup = `
+             <ul class='tabsUl helpController'>
+                <li>
+                     <div>
+                        <img src='scripts/HelpController/images/category.png'>
+                        <div class='tabsUl_div helpController'>Legend</div>
+                     </div>
+                </li>
+                <li>
+                     <div>
+                         <img src='scripts/HelpController/images/group_work.png'>
+                         <div class='tabsUl_div helpController'>Relationships</div>
+                     </div>
+                 </li>
+                 <li>
+                     <div >
+                         <img src='scripts/HelpController/images/mouse.png'>
+                         <div class='tabsUl_div helpController'>Navigation</div>
+                     </div>
+                 </li>
+             </ul>
+             <div></div>
+             <div></div>
+             <div></div>`;
+            
+         return jqxTabsPopup;     
+    }; 
+
+    function createPopup(){
+        var helpPopupDiv= "<div id='helpPopupDiv'></div>";
+        $("#DisplayWindow").remove();
+        var loadPopup = application.createPopup("Help",  
+                        helpPopupDiv, "DisplayWindow");
+                        document.body.appendChild(loadPopup);
+        $("#DisplayWindow").css("display", "block").jqxWindow({
+                                theme: "metro",
+                                width: 565,
+                                height: 645,
+                                isModal: true,
+                                autoOpen: true,
+                                resizable: false
+        });
+        return loadPopup; 
+    };
+    
+    function popupContent(){
+        return "<div id='helpPopupTabs'>" + jqxTabsPopup() + "</div>";
+    };
+            
+    function createTabs(){
+        $('#helpPopupTabs').jqxTabs({ 
+                    theme:'metro',
+                    width: 550,
+                    height: 600,
+                    position:'top'
+        });
+
+        $('#helpPopupTabs').jqxTabs('setContentAt', 0, variantMetaphor.legend());
+        $('#helpPopupTabs').jqxTabs('setContentAt', 1, variantMetaphor.relationships());
+        $('#helpPopupTabs').jqxTabs('setContentAt', 2, variantNavigation.navigation());
+        
+    };
+
+    function createPopupAndTabs(){
+        createPopup();
+        document.getElementById('helpPopupDiv').innerHTML= popupContent();
+        createTabs();
+    };
+    
+    function helpPopupContent(){
+        $.getScript("scripts/HelpController/navigationContent.js", function(){
+            createPopupAndTabs();
+        });
+    };
     
     return {
         initialize: initialize,
