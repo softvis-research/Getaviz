05f1475b50a812942f638f7409ac5158f48eb29b
Sophia <gaoxuefei55@gmail.com>
2019-12-18 12:43:18 +0100

    Improve code for ShareController

--- ui/scripts/ShareController/ShareController.js
+++ ui/scripts/ShareController/ShareController.js
@@ -1,109 +1,27 @@
 var shareController = (function() {
-    
-    //config parameters	
 	let controllerConfig = {
         showDebugOutput: false,
 	};
 
-    
     function initialize(setupConfig){   
-        
         application.transferConfigParams(setupConfig, controllerConfig);
-        
-		$(document).ready(function () {
-			$.getJSON( metaStateJsonUrl, initializeApplicationTimeout);
-		});
+    }    
 
-		function initializeApplicationTimeout(metaStateJson){
-			setTimeout(() => initializeApplication(metaStateJson), 2000);
-		}
-
-		function initializeApplication(metaStateJson){
-			let entities = [];
-			metaStateJson.selected.forEach(function(element){
-				var entity = model.getEntityById(element);
-				if (entity) {
-					entities.push(entity);
-				}
-			}); 
-		
-			var applicationEvent = {			
-				sender: shareController,
-				entities: entities
-			};
-			events.selected.on.publish(applicationEvent);
-
-			
-			entities = new Array();
-			metaStateJson.marked.forEach(function(element){
-				var entity = model.getEntityById(element);
-				if (entity) {
-					entities.push(entity);
-				}
-			});
-			
-			var applicationEvent = {			
-				sender: shareController,
-				entities: entities
-			};
-			events.marked.on.publish(applicationEvent);
-
-			entities = new Array();
-			metaStateJson.filtered.forEach(function(element){
-				var entity = model.getEntityById(element);
-				if (entity) {
-					entities.push(entity);
-				}
-			});
-			
-			var applicationEvent = {			
-				sender: shareController,
-				entities: entities
-			};
-			events.filtered.on.publish(applicationEvent);
-
-            if (visMode.includes( "x3dom")) {
-               //get reference of x3dom objects
-                var x3domRuntime = document.getElementById('x3dElement').runtime;
-                var viewarea = x3domRuntime.canvas.doc._viewarea;
-                var viewpoint = viewarea._scene.getViewpoint();
-
-                if (metaStateJson.viewMatrix) {
-                    Object.setPrototypeOf(metaStateJson.viewMatrix, x3dom.fields.SFMatrix4f.prototype);
-                    viewpoint.setView(metaStateJson.viewMatrix);
-                    viewarea._needNavigationMatrixUpdate = true;
-                }
-                if (metaStateJson.centerRotation) {
-                    Object.setPrototypeOf(metaStateJson.viewMatrix, x3dom.fields.SFVec4f.prototype);
-                    viewpoint.setCenterOfRotation(metaStateJson.centerRotation);
-                }
-           } else {
-                //get reference of aframe
-                var cameraEl = document.querySelector('#camera');
-
-                if (metaStateJson.position) {
-                    cameraEl.setAttribute('position', metaStateJson.position)
-                }
-                if (metaStateJson.rotation) {
-                    cameraEl.setAttribute('rotation', metaStateJson.rotation)
-                }
-           }
-		}
-    }
-	
 	function activate(){
-    
-        var id = "jqxTextImageButton";
-        var buttonType = "button";
-        var jqxTextImageButton = document.createElement("BUTTON");
-		jqxTextImageButton.type = buttonType;
-        jqxTextImageButton.id = id;
-        jqxTextImageButton.style = "float:right;";
+        createShareButton();
+        createSharePopup();
+	}
+
+    function createShareButton(){
+        var jqxShareButton = document.createElement("BUTTON");
+		jqxShareButton.type = "button";
+        jqxShareButton.id = "jqxShareButton";
+        jqxShareButton.style = "float:right;";
         var text = document.createTextNode("Share");
-        jqxTextImageButton.appendChild(text);
-       $("ul.jqx-menu-ul")[0].appendChild(jqxTextImageButton);
+        jqxShareButton.appendChild(text);
+        $("ul.jqx-menu-ul")[0].appendChild(jqxShareButton);
         
-        $("#jqxTextImageButton").jqxButton({ 
+        $("#jqxShareButton").jqxButton({ 
             theme: "metro",
             width: 80, 
             height: 25, 
@@ -111,128 +29,141 @@ var shareController = (function() {
             textPosition: "left", 
             imgSrc: "scripts/ShareController/images/share_icon.png" 
         });
+ 	}
 
+    function createSharePopup(){
+        $("#jqxShareButton").on('click', function (){
+            createPopup();
+            createCopyLinkButton();
+            createStatePHP();
+        });
         
-        $("#jqxTextImageButton").on('click', function (){
-            
-            var cssLink = document.createElement("link");
-            cssLink.type = "text/css";
-            cssLink.rel = "stylesheet";
-            cssLink.href = "scripts/ShareController/sharePopup.css";
-            
-            document.getElementsByTagName("head")[0].appendChild(cssLink);
-            var state = {
-                "selected": []
-                ,
-                "marked": []
-                ,
-                "filtered": []
-            };
-            var selectedEntities = events.selected.getEntities();
-            state.selected = new Array();
-            selectedEntities.forEach(function(element){
-                state.selected.push(element.id);
-            });
-            var markedEntities = events.marked.getEntities();
-            state.marked = new Array();
-            markedEntities.forEach(function(element){
-                state.marked.push(element.id);
-            });
-            var filteredEntities = events.filtered.getEntities();
-            state.filtered = new Array();
-            filteredEntities.forEach(function(element){
-                state.filtered.push(element.id);
-            });
-            
-            if (visMode.includes( "x3dom")) {
-                //get reference of x3dom objects
-                var x3domRuntime = document.getElementById('x3dElement').runtime;
-                var viewarea = x3domRuntime.canvas.doc._viewarea;
-                var viewpoint = viewarea._scene.getViewpoint();
-                
-                state.viewMatrix = viewarea.getViewMatrix();
-                state.centerRotation = viewpoint.getCenterOfRotation();
-            }else{
-                //get reference of aframe
-                var cameraEl = document.querySelector('#camera');
-                var position = cameraEl.getAttribute('position');
-                var rotation = cameraEl.getAttribute('rotation');
-
-                state.position = position;
-                state.rotation = rotation;
-            };
-            
-
-            var stateHashcode = JSON.stringify(state).hashCode();
-            var jsonString = JSON.stringify(state,null,'\t');
-            
-            var url = window.location.toString().split("&state=")[0].split("?state=")[0];
-            if (url.includes( "?")) {
-                    var state_N= "&state=";
-                } else{
-                    var state_N= "?state=";
-                };
-                
-            var stateID= "<span class='shareController titleSpan'>StateID:</span>" + stateHashcode + "<br /><br />";
-            var descriptionText = "Use this URL to share the current state of the visualization."+ "<br />";
-            var shareLinkDiv= "<div id='shareLinkDiv' class='shareController'><input id='copyField' class='shareController' readonly value='" + url + state_N + stateHashcode
-                    +"'></div> ";
-            var jsonHtml= "<span class='shareController titleSpan'>JSON:</span> <pre class='shareController jsonHtmlPre'>"+jsonString+"</pre>";
-            var popup;
+    }
     
-            if(controllerConfig.showDebugOutput===true) {
-                popup = stateID + descriptionText + shareLinkDiv + jsonHtml;
-            } else{
-                popup = descriptionText + shareLinkDiv;
-            }; 
-
-            $("#DisplayWindow").remove();
-            var loadPopup = application.createPopup("Share Visualization",  
-            popup, "DisplayWindow");
-            document.body.appendChild(loadPopup);
-            $("#DisplayWindow").css("display", "block").jqxWindow({
-                    theme: "metro",
-                    width: 400,
-                    height: 80,
-                    isModal: true,
-                    autoOpen: true,
-                    resizable: false
-            });
+    function createStateContent(){
+        var cssLink = document.createElement("link");
+        cssLink.type = "text/css";
+        cssLink.rel = "stylesheet";
+        cssLink.href = "scripts/ShareController/style.css";
+        document.getElementsByTagName("head")[0].appendChild(cssLink);
             
-            var shareLinkid = "jqxshareLinkButton";
-            var shareLinkbuttonType = "button";
-            var jqxshareLinkButton = document.createElement("BUTTON");
-            jqxshareLinkButton.type = shareLinkbuttonType;
-            jqxshareLinkButton.id = shareLinkid;
-
-            document.getElementById('shareLinkDiv').appendChild(jqxshareLinkButton);
-
-            $("#jqxshareLinkButton").jqxButton({ 
-                theme: "metro",
-                imgSrc: "scripts/ShareController/images/copy_icon.png"
-            });
+        var state = {
+            "selected": [],
+            "marked": [],
+            "filtered": []
+        };
+        var selectedEntities = events.selected.getEntities();
+        state.selected = new Array();
+        selectedEntities.forEach(function(element){
+            state.selected.push(element.id);
+        });
+        var markedEntities = events.marked.getEntities();
+        state.marked = new Array();
+        markedEntities.forEach(function(element){
+            state.marked.push(element.id);
+        });
+        var filteredEntities = events.filtered.getEntities();
+        state.filtered = new Array();
+        filteredEntities.forEach(function(element){
+            state.filtered.push(element.id);
+        });
             
-           $("#jqxshareLinkButton").on('click', function (){
-                document.getElementById('copyField').select();
-                document.execCommand("copy");
-            });
-           
+        if (visMode.includes( "x3dom")) {
+            //get reference of x3dom objects
+            var x3domRuntime = document.getElementById('x3dElement').runtime;
+            var viewarea = x3domRuntime.canvas.doc._viewarea;
+            var viewpoint = viewarea._scene.getViewpoint();
+            state.viewMatrix = viewarea.getViewMatrix();
+            state.centerRotation = viewpoint.getCenterOfRotation();
+        }else{
+            //get reference of aframe
+            var cameraEl = document.querySelector('#camera');
+            var position = cameraEl.getAttribute('position');
+            var rotation = cameraEl.getAttribute('rotation');
+            state.position = position;
+            state.rotation = rotation;
+        };
+        
+        return state;
+    }  
+    
+    function createPopupContent(){
+        var stateHashcode = JSON.stringify(createStateContent()).hashCode();
+        var jsonString = JSON.stringify(createStateContent(),null,'\t');
+            
+        var url = window.location.toString().split("&state=")[0].split("?state=")[0];
+        if (url.includes( "?")) {
+            var state_Describe= "&state=";
+        } else{
+            var state_Describe= "?state=";
+        };
+                
+        var stateID= "<span class='shareController titleSpan'>StateID:</span>" + stateHashcode + "<br /><br />";
+        var descriptionText = "Use this URL to share the current state of the visualization."+ "<br />";
+        var shareLinkDiv= "<div id='shareLinkDiv' class='shareController'><input id='copyField' class='shareController' readonly value='" + url + state_Describe + stateHashcode +"'></div> ";
+        var jsonHtml= "<br /><span class='shareController titleSpan'>JSON:</span> <pre class='shareController jsonHtmlPre'>"+jsonString+"</pre>";
+            
+        if(controllerConfig.showDebugOutput===true) {
+            var popup = stateID + descriptionText + shareLinkDiv + jsonHtml;
+        } else{
+            var popup = descriptionText + shareLinkDiv;
+        }; 
+        return popup;
+    }
+    
+    function createCopyLinkButton(){
+        var shareLinkid = "jqxshareLinkButton";
+        var shareLinkbuttonType = "button";
+        var jqxshareLinkButton = document.createElement("BUTTON");
+        jqxshareLinkButton.type = shareLinkbuttonType;
+        jqxshareLinkButton.id = shareLinkid;
+
+        document.getElementById('shareLinkDiv').appendChild(jqxshareLinkButton);
+
+        $("#jqxshareLinkButton").jqxButton({ 
+            theme: "metro",
+            imgSrc: "scripts/ShareController/images/copy_icon.png"
+        });
+            
+        $("#jqxshareLinkButton").on('click', function (){
+            document.getElementById('copyField').select();
+            document.execCommand("copy");
+        });
+    }
+    
+    function createPopup(){
+        $("#DisplayWindow").remove();
+        var loadPopup = application.createPopup("Share Visualization",  
+        createPopupContent(), "DisplayWindow");
+        document.body.appendChild(loadPopup);
+        $("#DisplayWindow").css("display", "block").jqxWindow({
+            theme: "metro",
+            //width: 400,
+            width: 600,
+            //height: 80,
+            height: 600,
+            isModal: true,
+            autoOpen: true,
+            resizable: false
+        });
+    }
+
+    function createStatePHP(){
             var xhr = new XMLHttpRequest();
             var jsonData = "state.php";
             xhr.open("POST", jsonData, true);
             xhr.setRequestHeader("Content-Type", "application/json"); 
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log("successfull");			 
+                    console.log("successfull");			 
                 }
             }; 
             xhr.send(JSON.stringify({
-                stateHashcode: stateHashcode,
-                jsonString: jsonString,
+                stateHashcode: JSON.stringify(createStateContent()).hashCode(),
+                jsonString: JSON.stringify(createStateContent(),null,'\t'),
             }));
-        });
-	}
-
+    }   
+ 
 	String.prototype.hashCode = function() {
 		var hash = 0, i, chr;
 		if (this.length === 0) return hash;
@@ -246,7 +177,83 @@ var shareController = (function() {
 		else
 			return Math.abs(hash * 10) + 1;	
     };
+    
+    $(document).ready(function () {
+        $.getJSON( metaStateJsonUrl, initializeApplicationTimeout);
+    });
 
+    function initializeApplicationTimeout(metaStateJson){
+        setTimeout(() => initializeApplication(metaStateJson), 2000);
+    }
+
+    function initializeApplication(metaStateJson){
+        let entities = [];
+        metaStateJson.selected.forEach(function(element){
+            var entity = model.getEntityById(element);
+            if (entity) {
+                entities.push(entity);
+            }
+        }); 
+        var applicationEvent = {			
+            sender: shareController,
+            entities: entities
+        };
+        events.selected.on.publish(applicationEvent);
+
+        entities = new Array();
+        metaStateJson.marked.forEach(function(element){
+            var entity = model.getEntityById(element);
+            if (entity) {
+                entities.push(entity);
+            }
+        });
+        var applicationEvent = {			
+            sender: shareController,
+            entities: entities
+        };
+        events.marked.on.publish(applicationEvent);
+
+        entities = new Array();
+        metaStateJson.filtered.forEach(function(element){
+            var entity = model.getEntityById(element);
+            if (entity) {
+                entities.push(entity);
+            }
+        });
+        var applicationEvent = {			
+            sender: shareController,
+            entities: entities
+        };
+        events.filtered.on.publish(applicationEvent);
+
+        if (visMode.includes( "x3dom")) {
+            //get reference of x3dom objects
+            var x3domRuntime = document.getElementById('x3dElement').runtime;
+            var viewarea = x3domRuntime.canvas.doc._viewarea;
+            var viewpoint = viewarea._scene.getViewpoint();
+
+            if (metaStateJson.viewMatrix) {
+                Object.setPrototypeOf(metaStateJson.viewMatrix, x3dom.fields.SFMatrix4f.prototype);
+                viewpoint.setView(metaStateJson.viewMatrix);
+                viewarea._needNavigationMatrixUpdate = true;
+            }
+            if (metaStateJson.centerRotation) {
+                Object.setPrototypeOf(metaStateJson.viewMatrix, x3dom.fields.SFVec4f.prototype);
+                viewpoint.setCenterOfRotation(metaStateJson.centerRotation);
+            }
+        } else {
+            //get reference of aframe
+            var cameraEl = document.querySelector('#camera');
+
+            if (metaStateJson.position) {
+                cameraEl.setAttribute('position', metaStateJson.position)
+            }
+            if (metaStateJson.rotation) {
+                cameraEl.setAttribute('rotation', metaStateJson.rotation)
+            }
+        }
+    }
+    
     return {
         initialize: initialize,
 		activate: activate
