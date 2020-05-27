cccb45753379f3d0756481981fb0729c262c354f
Janik Eriksson <janik.eriksson@icloud.com>
2020-05-27 23:22:30 +0200

    Remove unused imports

--- generator2/org.getaviz.generator/src/test/java/org/getaviz/generator/ArgoUmlTest.java
+++ generator2/org.getaviz.generator/src/test/java/org/getaviz/generator/ArgoUmlTest.java
@@ -1,30 +1,15 @@
 package org.getaviz.generator;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.getaviz.generator.database.DatabaseConnector;
-import org.getaviz.generator.extract.Importer;
-import org.getaviz.generator.extract.ScanStep;
 import org.getaviz.generator.mockups.ArgoUml;
-import org.getaviz.generator.mockups.Bank;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.neo4j.driver.v1.Record;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.kernel.configuration.BoltConnector;
-
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArgoUmlTest {
 
