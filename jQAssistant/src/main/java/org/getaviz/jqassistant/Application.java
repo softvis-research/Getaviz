3edfd04f87f05207c2d12b45b86680d1e5328772
David Baum <david.baum@uni-leipzig.de>
2020-07-16 15:23:33 +0200

    do not manual start jqa service on startup

--- jQAssistant/src/main/java/org/getaviz/jqassistant/Application.java
+++ jQAssistant/src/main/java/org/getaviz/jqassistant/Application.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		//JQAssistantService service = new JQAssistantService();
+	//	service.startServer();
 	}
 }
