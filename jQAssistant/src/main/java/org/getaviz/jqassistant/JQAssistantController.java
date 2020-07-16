f955d1af6d620f6c807a4c6d06af509eb81eb3b7
David Baum <david.baum@uni-leipzig.de>
2020-07-16 15:23:52 +0200

    split jQA into controller and service

--- jQAssistant/src/main/java/org/getaviz/jqassistant/JQAssistantController.java
+++ jQAssistant/src/main/java/org/getaviz/jqassistant/JQAssistantController.java
@@ -1,60 +1,38 @@
 package org.getaviz.jqassistant;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 public class JQAssistantController {
-    private final Log log = LogFactory.getLog(this.getClass());
-    private final Runtime runtime = Runtime.getRuntime();
-    private final String pathJQAssistant = "/opt/jqassistant/bin/jqassistant.sh";
-    private static Process serverProcess = null;
+
+    static JQAssistantService service = new JQAssistantService();
 
     @PostMapping("/server/start")
     public void startServer() {
-        log.info("Starting jQAssistant server");
-        try {
-            if(serverProcess == null) {
-                serverProcess = runtime.exec(pathJQAssistant + " server -embeddedListenAddress 0.0.0.0");
-            }
-        } catch (IOException e) {
-            log.error(e);
-            e.printStackTrace();
-        }
-        log.info("Starting jQAssistant server finished.");
+        service.startServer();
     }
 
     @PostMapping("/server/stop")
     public void stopServer() {
-        log.info("Stopping jQAssistant server");
-        if(serverProcess != null) {
-            serverProcess.destroy();
-            serverProcess = null;
-        }
-        log.info("jQAssistant server stopped.");
+        JQAssistantService.stopServer();
+    }
+
+    @GetMapping("/server/isRunning")
+    public boolean isRunning() {
+        return JQAssistantService.isRunning();
     }
 
     @PostMapping("/scan/{url}")
     public void startScan (@PathVariable String url) {
-        log.info("jQAssistant scan started.");
-        log.info("Scanning from URI(s) " + decodeUrl(url));
-        try {
-            stopServer();
-            String options = "scan -reset -u " + decodeUrl(url);
-            Process pScan = runtime.exec(pathJQAssistant + " " + options);
-            pScan.waitFor();
-            startServer();
-        } catch (IOException | InterruptedException e) {
-            log.error(e);
-            e.printStackTrace();
-        }
-        log.info("jQAssistant scan finished.");
+        JQAssistantService service = new JQAssistantService();
+        service.scan(decodeUrl(url));
     }
 
     public static String decodeUrl(String encodedUrl) {
