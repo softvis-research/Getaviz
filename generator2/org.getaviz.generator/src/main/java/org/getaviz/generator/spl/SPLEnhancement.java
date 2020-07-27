9fcc2d0d36b6f70a97c7c35673d7d0538b795d84
Janik Eriksson <janik.eriksson@icloud.com>
2020-07-27 20:28:38 +0200

    Get fully qualified name of feature traces

--- generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/spl/SPLEnhancement.java
+++ generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/spl/SPLEnhancement.java
@@ -3,9 +3,13 @@ package org.getaviz.generator.spl;
 import org.getaviz.generator.Step;
 import org.getaviz.generator.database.DatabaseConnector;
 
+import java.io.FileReader;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
 public class SPLEnhancement implements Step {
 
-	private DatabaseConnector connector = DatabaseConnector.getInstance();
+	//private DatabaseConnector connector = DatabaseConnector.getInstance();
 	//private 
 	
 	@Override
@@ -23,8 +27,30 @@ public class SPLEnhancement implements Step {
 	}
 
 	private void enhanceClasses() {
-		BenchmarkFileReader reader = new BenchmarkFileReader();
-		reader.read();
+		BenchmarkFileReader bfReader = new BenchmarkFileReader();
+		bfReader.read();
+		String path = "C:\\Users\\Janik\\Documents\\Bachelorarbeit\\getaviz\\generator2\\org.getaviz.generator\\test\\output\\ArgoUmlTest\\default\\model\\metaData.json";
+		JSONParser parser = new JSONParser();
+		try { 
+			FileReader reader = new FileReader(path);
+			JSONArray arr = (JSONArray) parser.parse(reader);
+			for (Object obj: arr) {
+				JSONObject jsonObj = (JSONObject) obj;
+				String qualifiedName = jsonObj.get("qualifiedName").toString();
+				int index = qualifiedName.indexOf("(");
+				if (index >= 0) {
+					qualifiedName = qualifiedName.substring(0, index);
+				}
+				System.out.println(qualifiedName);
+			}
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static void main(String[] args) {
+		SPLEnhancement spl = new SPLEnhancement();
+		spl.enhanceClasses();
 	}
 
 }
