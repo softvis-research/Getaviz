1b3f71bfb9d77cc4e7eddaafbb3e0c49ba726f6c
ToniQu <antonia.quaas@gmx.de>
2020-09-17 10:18:22 +0200

    new ABAPNavigationController

---
+++ ui/scripts/ABAPNavigation/ABAPNavigationController.js
@@ -0,0 +1,30 @@
+var abapNavigationController = (function() {
+    function initialize(){
+    }
+	
+	function activate(){
+		window.addEventListener("wheel", event => {
+            const delta = Math.sign(event.wheelDelta) * 0.1;
+            //getting the mouse wheel change (120 or -120 and normalizing it to 1 or -1)
+            var mycam=document.getElementById('cam').getAttribute('camera');
+            var finalZoom=document.getElementById('cam').getAttribute('camera').zoom+delta;
+            //limiting the zoom so it doesnt zoom too much in or out
+            if(finalZoom<1)
+              finalZoom=1;
+            if(finalZoom>5)
+              finalZoom=5; 
+            if(finalZoom>10)
+              finalZoom=10;   
+        
+            mycam.zoom=finalZoom;
+            //setting the camera element
+            document.getElementById('cam').setAttribute('camera',mycam);
+          });
+    }
+
+
+    return {
+        initialize: initialize,
+		activate: activate
+    };  
+})();
