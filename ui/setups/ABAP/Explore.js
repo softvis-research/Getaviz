f430b926c6017fcea639376131231ff18eeb96f2
ToniQu <45816049+ToniQu@users.noreply.github.com>
2020-11-03 09:58:59 +0100

    removed Banner-, Legend- & Select-Controller

--- ui/setups/ABAP/Explore.js
+++ ui/setups/ABAP/Explore.js
@@ -9,131 +9,6 @@ var setup = {
         {
             name: "canvasHoverController"
         },
-        {
-            name: "canvasSelectController"
-        },
-
-        {
-            name: "bannerController"
-        },
-        
-        {   name: "legendController",
-            entries: [{
-                name: "Elements",
-                open: true,
-                icon: "scripts/Legend/images/category.png",
-                entries: [{
-                    name: "Package",
-                    open: false,
-                    icon: "scripts/Legend/images/circle_gray.png",
-                },{
-                    name: "Class",
-                    open: false,
-                    icon: "scripts/Legend/images/circle_yellow.png",
-                    entries: [{
-                        name: "Method",
-                        icon: "scripts/Legend/images/cuboid_gray.png",
-                        },{
-                        name: "Attribute",
-                        icon: "scripts/Legend/images/cylinder_black.png",
-                        },{
-                        name: "LocalClass",
-                        icon: "scripts/Legend/images/circle_yellow_light.png",
-                        },{
-                        name: "LocalInterface",
-                        icon: "scripts/Legend/images/circle_red_light.png",
-                    }]
-                },{
-                    name: "Interface",
-                    open: false,
-                    icon: "scripts/Legend/images/circle_red.png",
-                    entries: [{
-                        name: "Method",
-                        open: false,
-                        icon: "scripts/Legend/images/cuboid_gray.png",
-                    },{
-                        name: "Attribute",
-                        open: false,
-                        icon: "scripts/Legend/images/cylinder_black.png",
-                    },{
-                        name: "LocalClass",
-                        icon: "scripts/Legend/images/circle_yellow_light.png",
-                    },{
-                        name: "LocalInterface",
-                        icon: "scripts/Legend/images/circle_red_light.png",
-                    }]
-                },{
-                    name: "Report",
-                    open: false, 
-                    icon: "scripts/Legend/images/circle_blue_light.png",
-                    entries: [{
-                        name: "Report Building",
-                        icon: "scripts/Legend/images/cuboid_blue.png",
-                    },{
-                        name: "Formroutine",
-                        icon: "scripts/Legend/images/cuboid_gray.png",
-                    },{
-                        name: "Attribute",
-                        icon: "scripts/Legend/images/cylinder_black.png",
-                    },{
-                        name: "LocalClass",
-                        icon: "scripts/Legend/images/circle_yellow_light.png",
-                    },{
-                        name: "LocalInterface",
-                        icon: "scripts/Legend/images/circle_red_light.png",
-                    }]
-                },{
-                    name: "Function Group",
-                    open: false, 
-                    icon: "scripts/Legend/images/circle_violet.png",
-                    entries: [{
-                        name: "Function Module",
-                        icon: "scripts/Legend/images/cuboid_gray.png",
-                    },{
-                        name: "Formroutine",
-                        icon: "scripts/Legend/images/cuboid_gray.png",
-                    },{
-                        name: "Attribute",
-                        icon: "scripts/Legend/images/cylinder_black.png",
-                    },{
-                        name: "LocalClass",
-                        icon: "scripts/Legend/images/circle_yellow_light.png",
-                    },{
-                        name: "LocalInterface",
-                        icon: "scripts/Legend/images/circle_red_light.png",
-                    }]
-                },{
-                    name: "ReferenceBuildings",
-                    open: true,
-                    icon: "scripts/Legend/images/circle_width.png",
-                    entries:[{
-                            name: "Mountain",
-                            icon: "scripts/Legend/images/mountain.png",
-                        },{
-                            name: "Cloud",
-                            icon: "scripts/Legend/images/cloud_black.png",
-                        },{
-                            name: "Lake",
-                            icon: "scripts/Legend/images/lake.png",
-                    }]
-                }]
-                },{
-                name: "Navigation",
-                open: true,
-                icon: "scripts/Legend/images/mouse.png",
-                entries: [{
-                        name: "Move",
-                        icon: "scripts/Legend/images/left.png",
-                    },{
-                        name: "Zoom",
-                        icon: "scripts/Legend/images/scrolling.png",
-                    },{
-                        name: "Rotate",
-                        icon: "scripts/Legend/images/right.png",
-                }]
-                
-            }],
-		},
     ],
 
 
@@ -146,31 +21,24 @@ var setup = {
                 orientation: "horizontal",
                 resizable: false,
                 first: {
-                    size: "75px",
-                    name: "banner",
+                    size: "10%",
                     collapsible: true,
                     controllers: [
-                        {name: "bannerController" },
+                        {},
                         ],
                     },
                 second: {
-                    size: "100%",
+                    size: "90%",
                     collapsible: false,
                     area: {
                         orientation: "vertical",
                         name: "leftPanel",
                         first: {                            
 							size: "10%",
-							expanders: [
-								{
-									name: "legend",
-									title: "Legend",
-									controllers: [
-										{ name: "legendController" }
-									],
-								},
-							]
-						},
+							controllers: [
+                                {},
+                                ],
+                        },
                         second: {
 							size: "90%",
 							collapsible: false,
@@ -179,7 +47,6 @@ var setup = {
 							controllers: [
 								{ name: "defaultLogger" },
                                 { name: "canvasHoverController" },
-                                { name: "canvasSelectController" },
 							],
                         }
                     }
