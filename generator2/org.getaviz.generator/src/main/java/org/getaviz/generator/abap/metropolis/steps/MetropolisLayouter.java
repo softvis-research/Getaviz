caa9bb5ba2b296fa1cbb51fdce3ff2735e7f603e
PascalKovacs <Ef4f?n.L14iv>
2020-12-08 14:04:23 +0100

    Refactoring Layout Step

--- generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/abap/metropolis/steps/MetropolisLayouter.java
+++ generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/abap/metropolis/steps/MetropolisLayouter.java
@@ -16,9 +16,7 @@ import org.neo4j.driver.v1.Value;
 import org.neo4j.driver.v1.types.Node;
 
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MetropolisLayouter {
 
@@ -43,20 +41,27 @@ public class MetropolisLayouter {
 
     public void layoutRepository(){
 
+        //layout buildings
         Collection<ACityElement> buildings = repository.getElementsByType(ACityElement.ACityType.Building);
         log.info(buildings.size() + " buildings loaded");
-
         layoutBuildings(buildings);
 
+        //layout reference elements
         Collection<ACityElement> referenceElements = repository.getElementsByType(ACityElement.ACityType.Reference);
         log.info(referenceElements.size() + " reference elements loaded");
-
         layoutReferenceElements(referenceElements);
 
+        //layout empty districts
+        Collection<ACityElement> emptyDistrict = getEmptyDistricts();
+        layoutEmptyDistricts(emptyDistrict);
 
-        layoutEmptyDistricts(buildings);
-        layoutParentDistricts(buildings);
+        //layout districts
+        Collection<ACityElement> layoutedSubElements = buildings;
+        layoutedSubElements.addAll(emptyDistrict);
 
+        layoutParentDistricts(layoutedSubElements);
+
+        //layout cloud elements
         layoutCloudModel();
 
     }
@@ -69,115 +74,32 @@ public class MetropolisLayouter {
 
     private void layoutBuildings(Collection<ACityElement> buildings) {
         for (ACityElement building: buildings) {
-
-            SAPNodeTypes sourceNodeType = building.getSourceNodeType();
-
-            if(sourceNodeType == SAPNodeTypes.TableType) {
-                SAPNodeTypes buildingSourceType = getTableTypeTypeOfType(building);
-
-                if (buildingSourceType != null) {
-                    layoutTableTypeBuilding(building, buildingSourceType);
-
-                    log.info(building.getSourceNodeProperty(SAPNodeProperties.type_name) + " \""
-                            + building.getSourceNodeProperty(SAPNodeProperties.object_name) + "\""
-                            + " with rowType " + "\"" + building.getSourceNodeProperty(SAPNodeProperties.rowtype) + "\""
-                            + " layouted");
-                }
-            } else {
-                layoutBuilding(building);
-            }
+           layoutBuilding(building);
         }
     }
 
-    private SAPNodeTypes getTableTypeTypeOfType(ACityElement building) {
 
-        Node tableTypeSourceNode = building.getSourceNode();
+    private Collection<ACityElement> getEmptyDistricts(){
 
-        Collection<Node> typeOfNodes = nodeRepository.getRelatedNodes(tableTypeSourceNode, SAPRelationLabels.TYPEOF, true);
-        if(typeOfNodes.isEmpty()){
-            String tableTypeName = building.getSourceNodeProperty(SAPNodeProperties.object_name);
-            log.warn("TYPEOF related nodes not found for tableType \"" + tableTypeName + "\"");
-            return null;
-        }
-        if(typeOfNodes.size() != 1){
-            String tableTypeName = building.getSourceNodeProperty(SAPNodeProperties.object_name);
-            log.error("TYPEOF related nodes more than 1 for tableType \"" + tableTypeName + "\"");
-            return null;
-        }
-
-        for (Node typeOfNode: typeOfNodes) {
-            Value propertyValue = typeOfNode.get(SAPNodeProperties.type_name.name());
-            String typeOfNodeTypeProperty = propertyValue.asString();
-
-            return SAPNodeTypes.valueOf(typeOfNodeTypeProperty);
-        }
-
-        return null;
-    }
-
-    private void layoutTableTypeBuilding(ACityElement building, SAPNodeTypes typeOfType) {
-
-        switch (typeOfType){
-            case Class:
-            case Interface:
-                building.setHeight(config.getACityTableTypeBuildingHeight("tableTypeBuilding_class"));
-                break;
-            case Table:
-            case TableType:
-                building.setHeight(config.getACityTableTypeBuildingHeight("tableTypeBuilding_table"));
-                break;
-            case Structure:
-                building.setHeight(config.getACityTableTypeBuildingHeight("tableTypeBuilding_structure"));
-                break;
-            case DataElement:
-                building.setHeight(config.getACityTableTypeBuildingHeight("tableTypeBuilding_dateElement"));
-            default:
-                building.setHeight(1);
-                String tableTypeName = building.getSourceNodeProperty(SAPNodeProperties.type_name);
-                log.error("Type \"" + typeOfType + "\" not allowed for tableType-Element \"" +  tableTypeName );
-        }
-
-
-        Double groundAreaLength = config.getACityGroundAreaByChimneyAmount();
-        building.setWidth(groundAreaLength);
-        building.setLength(groundAreaLength);
-
-        building.setXPosition(0.0);
-        building.setYPosition(building.getHeight() / 2);
-        building.setZPosition(0.0);
-    }
-
-    private String getRowtype(ACityElement aCityElement){
-
-        if (aCityElement.getSourceNodeProperty(SAPNodeProperties.type_name).equals(SAPNodeTypes.TableType.name())){
-            if (aCityElement.getSourceNodeProperty(SAPNodeProperties.rowtype) == null) {
-                return "TableType doesn't have a rowType";
-            }
-
-        }
-        return aCityElement.getSourceNodeProperty(SAPNodeProperties.rowtype);
-    }
-
-    private void layoutEmptyDistricts( Collection<ACityElement> districtElements) {
+        Collection<ACityElement> emptyDistricts = new ArrayList<>();
 
         Collection<ACityElement> districts = repository.getElementsByType(ACityElement.ACityType.District);
 
         for (ACityElement district: districts) {
 
             if (district.getSubElements().isEmpty()) {
-
-                district.setHeight(config.getMetropolisEmptyDistrictHeight());
-                district.setLength(config.getMetropolisEmptyDistrictLength());
-                district.setWidth(config.getMetropolisEmptyDistrictWidth());
-
-                ACityElement parentElementOfEmptyDistricts = district.getParentElement();
-
-                if (parentElementOfEmptyDistricts != null) {
-
-                    districtElements.add(district);
-
-                }
+                emptyDistricts.add(district);
             }
+        }
+
+        return emptyDistricts;
+    }
+
+    private void layoutEmptyDistricts( Collection<ACityElement> districts) {
+        for (ACityElement district: districts) {
+            district.setHeight(config.getMetropolisEmptyDistrictHeight());
+            district.setLength(config.getMetropolisEmptyDistrictLength());
+            district.setWidth(config.getMetropolisEmptyDistrictWidth());
         }
     }
 
