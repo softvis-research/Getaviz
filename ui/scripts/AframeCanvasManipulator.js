2261d38ed8836360011f7fb103f9161e6e67227c
FabiDann <f.dannegger@web.de>
2021-03-02 11:40:39 +0100

    Clean up Code in AframeCanvasManipulator

--- ui/scripts/AframeCanvasManipulator.js
+++ ui/scripts/AframeCanvasManipulator.js
@@ -55,7 +55,6 @@ var canvasManipulator = (function () {
                 material: { type: 'string', default: '' },
                 shadow: { type: 'string', default: '' },
                 radius: { type: 'string', default: '' }
-                // component.getAtrributeNames() -> ["id", "position", "height", "width", "depth", "color",  ++++ Unknown Properties: "shadow", "material", "geometry"] ("radius")
             },
 
             init: function () {
@@ -64,19 +63,9 @@ var canvasManipulator = (function () {
                 Object.keys(this.schema).forEach(key => {
                     this.el.setAttribute(`${key}`, this.data[key]);
                 })
-
-                //position = AFRAME.utils.coordinates.parse(this.data['position'].nodeValue);
-                //this.el.attributes['position'] = position;
-
-
-                //document.getElementById(this.data["id"]).setAttribute("position", this.data["position"]); // this does set position as a workaround
-                //document.getElementById(this.data["id"]).setAttribute("position", this.data["position"]);
-                //this.el.setAttribute("position", this.data["position"]);
-                // Adjust visibility
-                // let visible = this.data['checked'] ? true : false; // set to visible, because parent node was checked
-                //this.el.setAttribute('visible', true);
             }
         });
+        
         AFRAME.registerComponent('set-position-attribute', {
             schema: {
                 position: { type: 'vec3'}
@@ -84,19 +73,9 @@ var canvasManipulator = (function () {
 
             init: function () {
 
-                var position = AFRAME.utils.coordinates.parse(this.data['position'].nodeValue);
+                let position = AFRAME.utils.coordinates.parse(this.data['position'].nodeValue);
                 this.el.setAttribute("position", position);
                 this.el.flushToDOM();
-                //this.el.setAttribute("position", this.data['position'].value);
-
-                //this.el.attributes[position] = this.data['position'].nodeValue;
-
-
-                //Funktionieren nicht
-                //document.getElementById(this.data["id"]).setAttribute("position", this.data["position"]); // this does set position as a workaround
-                //document.getElementById(this.data["id"]).setAttribute("position", this.data["position"]);
-                //this.el.setAttribute("position", this.data["position"]);
-                //this.el.attributes['position'] = position;
             }
         });
     }
@@ -374,7 +353,6 @@ var canvasManipulator = (function () {
     }
 
     function hideEntities(entities, controller) {
-        //changeVisibilityOfEntities(entities, false, controller);
 
         entities.forEach(function (entity) {
 
@@ -386,18 +364,11 @@ var canvasManipulator = (function () {
 
             hiddenEntitiesMap.set(entity.id, component);
             component.remove();
-            //scene.removeChild(component);
-
-            //component.parentNode.removeChild(component);
-            //component.remove();
         });
 
     }
 
     function showEntities(entities, controller) {
-        //changeVisibilityOfEntities(entities, true, controller);
-
-
 
         entities.forEach(function (entity) {
 
@@ -409,79 +380,15 @@ var canvasManipulator = (function () {
             let component = hiddenEntitiesMap.get(entity.id);
             hiddenEntitiesMap.delete(entity.id);
 
-            var attributes = {};
+            let attributes = {};
             component.getAttributeNames().forEach(function(attribute){attributes[attribute] = component.getAttribute(attribute)});
             attributes['position'] = component.attributes['position']; // needed as a fix for a bug with getting position
-            /*if(attributes['set-position-attribute']) {
-                attributes['position'] = component.attributes['object Object'];
-            }
-            else {
-                attributes['position'] = component.attributes['position'];
-            }*/
-
-
 
             let entityEl = document.createElement(component.tagName);
             entityEl.setAttribute('set-aframe-attributes', {...attributes});  // these attributes will be set after element is created
             entityEl.setAttribute('set-position-attribute', {position: attributes['position']}); // needed as a fix for an AFRAME bug with setting position
             let sceneEl = document.querySelector('a-scene');
             sceneEl.appendChild(entityEl);
-            //document.getElementById(entity.id).setAttribute("position", component.getAttribute("position")); // this does set position as a workaround
-            //Funktioniert hier nicht, da document.getElementById(entity.id) noch null ist
-
-            //TODO Create Copy of A-Frame Element
-            //https://aframe.io/docs/0.7.0/introduction/javascript-events-dom-apis.html
-
-            //Position Workaround
-            //https://stackoverflow.com/questions/41336889/adding-new-entities-on-the-fly-in-aframe
-        });
-    }
-
-    function changeVisibilityOfEntities(entities, targetValue, controller) {
-        entities.forEach(function (entity) {
-            if (entity === undefined) {
-                return;
-            }
-
-            let component = document.getElementById(entity.id);
-            if (component === undefined) {
-                events.log.error.publish({ text: "CanvasManipulator - changeVisibilityOfEntities - components for entityIds not found" });
-                return;
-            }
-
-            updateEntityEffectMap(entity, "visibility");
-
-            var visibilityList = entityEffectMap.get(entity.id).get("visibility");
-
-            var visible = component.object3D.visible;
-
-            if (visibilityList.length === 0) {
-                visibilityList.push(
-                    {
-                        controller: "original",
-                        value: visible
-                    }
-                );
-            } else {
-                var visibilityEffectIndex = visibilityList.findIndex(visibilityEffect => visibilityEffect.controller == controller.name);
-
-                //remove old visibility entry of this controller
-                if (visibilityEffectIndex !== -1) {
-                    visibilityList.splice(visibilityEffectIndex, 1);
-                }
-            }
-
-            visibilityList.push(
-                {
-                    controller: controller.name,
-                    value: targetValue
-                }
-            );
-
-            //length is always at least 2 (original value and target value)
-            if (visible !== targetValue) {
-                setVisibility(component, targetValue);
-            }
         });
     }
 
