a80be03f30f26533a1cfc5c0a7044f7fb78f24c2
Max <max.maercz98@gmail.com>
2022-01-05 23:02:59 +0100

    Classes for road graph and road node

---
+++ generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/abap/layouts/road/network/RoadGraph.java
@@ -0,0 +1,9 @@
+package org.getaviz.generator.abap.layouts.road.network;
+
+public class RoadGraph {
+
+	public RoadGraph() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ generator2/org.getaviz.generator/src/main/java/org/getaviz/generator/abap/layouts/road/network/RoadNode.java
@@ -0,0 +1,9 @@
+package org.getaviz.generator.abap.layouts.road.network;
+
+public class RoadNode {
+
+	public RoadNode() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
